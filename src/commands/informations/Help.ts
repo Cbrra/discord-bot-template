ecc7738acb48c2c752d3824acd8104f26a5fe574
Cbrra <98268014+Cbrra@users.noreply.github.com>
2022-12-30 17:39:00 +0100

    Create Help.ts

---
+++ src/commands/informations/Help.ts
@@ -0,0 +1,44 @@
+import { EmbedBuilder, SlashCommandBuilder, type ChatInputCommandInteraction } from "discord.js";
+import type Bot from "@structures/Bot";
+import { Command, CommandCategory } from "@structures/Command";
+
+class HelpCommand extends Command {
+    public static readonly data = {
+        name: "help",
+        category: CommandCategory.Informations,
+        slashBuilder: new SlashCommandBuilder()
+            .setName("help")
+            .setDescription("Get the commands list")
+    }
+
+    public constructor(client: Bot<true>) {
+        super(client, HelpCommand.data);
+    }
+
+    public async execute(interaction: ChatInputCommandInteraction) {
+        const fields: [CommandCategory, string[]][] = [];
+
+        for(const c of this.client.commands.values()) {
+            const index = fields.findIndex(f => f[0] == c.data.category);
+            const text = `${c.data.name}${c.disabled ? " (disabled)" : ""}`;
+
+            if(index != -1) fields[index][1].push(text);
+            else fields.push([c.data.category, [text]]);
+        }
+
+        const embed = new EmbedBuilder()
+            .setColor(0xffffff)
+            .setTitle("Commands list")
+            .setFields(
+                fields.map(f => ({
+                          // name -> Name
+                    name: `${f[0][0].toUpperCase()}${f[0].slice(1)}`,
+                    value: f[1].join("\n")
+                }))
+            );
+
+        await interaction.reply({ embeds: [embed] })
+    }
+}
+
+export default HelpCommand;
