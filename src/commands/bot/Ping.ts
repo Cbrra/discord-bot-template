4b24ff3bb0872b9ca0646b713e3a213bfe34f5a7
Cbrra <98268014+Cbrra@users.noreply.github.com>
2022-12-30 17:38:37 +0100

    Update Ping.ts

--- src/commands/bot/Ping.ts
+++ src/commands/bot/Ping.ts
@@ -1,10 +1,19 @@
 import { SlashCommandBuilder, type ChatInputCommandInteraction } from "discord.js";
-import { Command } from "@structures/Command";
+import type Bot from "@structures/Bot";
+import { Command, CommandCategory } from "@structures/Command";
 
 class PingCommand extends Command {
-    public static readonly data = new SlashCommandBuilder()
-        .setName("ping")
-        .setDescription("Get the bot latency");
+    public static readonly data = {
+        name: "ping",
+        category: CommandCategory.Bot,
+        slashBuilder: new SlashCommandBuilder()
+            .setName("ping")
+            .setDescription("Get the bot latency")
+    }
+
+    public constructor(client: Bot<true>) {
+        super(client, PingCommand.data);
+    }
 
     public async execute(interaction: ChatInputCommandInteraction) {
         await interaction.reply(`WebSocket latency: ${this.client.ws.ping}ms`);
